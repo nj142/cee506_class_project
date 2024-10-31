d6d2144a86cb50b9c4ef2115838d2d65f08d900c
hana0913 <hana.kawashima@duke.edu>
2024-10-31 10:30:15 -0400

    Update enso_indices.py

--- Scripts/enso_indices.py
+++ Scripts/enso_indices.py
@@ -1,1 +1,42 @@
 
+#Added comments
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import re
+from datetime import datetime
+import json
+
+enso_data_filepath = '/home/jovyan/work/ESDA_project_past/ENSO_index.rtf'
+
+def ReturnENSOFetch(years_of_data, enso_data_filepath):
+
+    #Open the file
+    with open(enso_data_filepath, 'r', encoding='utf-8') as rtf_file:
+        rtf_content = rtf_file.read()
+
+    #(\d+): A sequence of digits
+    #\s+: One or more white space
+    #[-+]?: + or - in the beginning
+    #\d*\.\d+: Floating point numbers
+    #\d+: Integer numbers
+    #data = re.findall(r'(\d+)\s+([-+]?\d*\.\d+|\d+)(?:\s+([-+]?\d*\.\d+|\d+))*', rtf_content)
+    data = re.findall(r'(\d+)\s+((?:[-+]?\d*\.\d+|\d+)(?:\s+[-+]?\d*\.\d+|\s+\d+)*)', rtf_content)
+    data_processed = [(year, list(map(float, months.split()))) for year, months in data]
+
+    #Extract data
+    ENSO_index = {}
+
+    #def ReturnENSOFetch(years_of_data):
+    for year in years_of_data:
+        #print(year)
+        ENSO_index_year = []
+        for y, m in [(year - 1, month) for month in range(3, 13)] + [(year, month) for month in range(1, 4)]:
+            value = ENSO_index.get((y, m))
+            if value is not None:
+                date_key = datetime(y, 3, 1)
+                ENSO_index_year.append(value)
+        #print(ENSO_index)
+        #ENSO_index.append(ENSO_index_year)
+        ENSO_index[year] = ENSO_index_year
+    return ENSO_index
